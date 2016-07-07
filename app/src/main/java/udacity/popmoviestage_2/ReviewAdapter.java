02a77fdcc404be008670a6fdb56b51f4696a9789
yeahecho <guoecho@hotmail.com>
2016-07-07 10:38:36 -0400

    Popular movies stage-2

--- app/src/main/java/udacity/popmoviestage_2/ReviewAdapter.java
+++ app/src/main/java/udacity/popmoviestage_2/ReviewAdapter.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -40,16 +40,21 @@ public class ReviewAdapter extends ArrayAdapter<Review> {
             LayoutInflater inflater = ((Activity)context).getLayoutInflater();
             view = inflater.inflate(resource, parent, false);
             holder = new ViewHolder();
-            holder.listView = (ListView) view.findViewById(R.id.lv_review);
+            holder.ReviewAuthor = (TextView) view.findViewById(R.id.author);
+            holder.ReviewContent = (TextView) view.findViewById(R.id.content);
             view.setTag(holder);
         }else{
             holder = (ViewHolder) view.getTag();
         }
         Review review = mReview.get(position);
+        holder.ReviewAuthor.setText(review.getAuthor());
+        holder.ReviewContent.setText(review.getReview());
+
         return view;
     }
 
     private class ViewHolder {
-        ListView listView;
+        TextView ReviewAuthor;
+        TextView ReviewContent;
     }
 }
--- app/src/main/java/udacity/popmoviestage_2/VideoAdapter.java
+++ app/src/main/java/udacity/popmoviestage_2/VideoAdapter.java
@@ -6,7 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -40,16 +41,21 @@ public class VideoAdapter extends ArrayAdapter<Video> {
             LayoutInflater inflater = ((Activity)context).getLayoutInflater();
             view = inflater.inflate(resource, parent, false);
             holder = new ViewHolder();
-            holder.listView = (ListView) view.findViewById(R.id.lv_review);
+            holder.VideoKey = (ImageView) view.findViewById(R.id.key);
+            holder.VideoName = (TextView) view.findViewById(R.id.name);
             view.setTag(holder);
         }else{
             holder = (ViewHolder) view.getTag();
         }
         Video video = mVideo.get(position);
+        System.out.println("======================name"+ video.getTrailerName());
+
+        holder.VideoName.setText(video.getTrailerName());
         return view;
     }
 
     private class ViewHolder {
-        ListView listView;
+        ImageView VideoKey;
+        TextView VideoName;
     }
 }
